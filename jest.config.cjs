abcb1d31c36949e3c00a58641b3f65c823781012
Daomtthuan <dao.mt.thuan@gmail.com>
2024-03-04 06:03:43 +0700

    Setup jest mock with meta data

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,26 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {},
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/mocks/file.ts',
